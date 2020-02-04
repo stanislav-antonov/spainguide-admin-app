4435060da6448680ad18a2a1a672eb83c10c87ba
Stanislav Antonov <stanislav.antonov@siemens.com>
2020-02-04 23:39:01 +0100

    Go on

--- src/Article.js
+++ src/Article.js
@@ -1,35 +1,51 @@
 import React, { Component } from "react";
-import { Editor } from '@tinymce/tinymce-react';
-import ImageUploader from 'react-images-upload';
+import { Editor } from "@tinymce/tinymce-react";
+import ImageUploader from "react-images-upload";
 
 class Article extends Component {
     constructor(props) {
 		super(props);
-		this.state = { images: [] };
-		this.onDrop = this.onDrop.bind(this);
+		this.state = { 
+            images: [],
+            headline: "",
+            alias: "",
+            title: "",
+            description: "",
+            preview: "",
+            content: "",
+            active: true
+        };
+        this.handleImageChange = this.handleImageChange.bind(this);
+        this.handleEditorChange = this.handleEditorChange.bind(this);
 	}
 
-	onDrop(imageFiles, imageDataURLs) {
-		this.setState({
-            images: this.state.images.concat(imageFiles),
-        });
+	handleImageChange(imageFiles, imageDataURLs) {
+        this.setState({images: this.state.images.concat(imageFiles)});
 	}
 
     handleEditorChange = (e) => {
-        console.log('Content was updated:', e.target.getContent());
+        this.setState({content: e.target.getContent()});
     }
 
     handleSave = (e) => {
         let id = this.props.match.params.id;
         let action = id ? "update/" + id  : "create";
         let endpointUrl = process.env.REACT_APP_SERVER_URL + "/api/admin/article/" + action;
+        
         let formData = new FormData();
-        formData.append('image', this.state.images[this.state.images.length - 1]);
+        formData.append("headline", this.state.headline);
+        formData.append("alias", this.state.alias);
+        formData.append("title", this.state.title);
+        formData.append("description", this.state.description);
+        formData.append("preview", this.state.preview);
+        formData.append("image", this.state.images[this.state.images.length - 1]);
+        formData.append("content", this.state.content);
+        formData.append("active", this.state.active);
 
-        console.log('Article save endpoint URL: ' + endpointUrl);
+        console.log("Article save endpoint URL: " + endpointUrl);
 
         fetch(endpointUrl, {
-            method: 'POST',
+            method: "POST",
             body: formData
         }).then(
             response => response.json()
@@ -39,7 +55,43 @@ class Article extends Component {
             error => console.log(error)
         );
 
-        console.log('Article was saved');
+        console.log("Article was saved");
+    }
+
+    handleHeadlineChange = (e) => {
+        this.setState({headline: e.target.value});
+    }
+
+    handleAliasChange = (e) => {
+        this.setState({alias: e.target.value});
+    }
+
+    handleTitleChange = (e) => {
+        this.setState({title: e.target.value});
+    }
+    
+    handleDescriptionChange = (e) => {
+        this.setState({description: e.target.value});
+    }
+
+    handlePreviewChange = (e) => {
+        this.setState({preview: e.target.value});
+    }
+
+    handleActiveChange = (e) => {
+        this.setState({active: e.target.value});
+    }
+
+    componentDidMount() {
+        this.setState({
+            headline: "Loaded Headline",
+            alias: "Loaded Alias",
+            title: "Loaded Title",
+            description: "Loaded Description",
+            preview: "Loaded Preview",
+            content: "Loaded Content",
+            active: false,
+        });
     }
 
     render() {
@@ -48,19 +100,26 @@ class Article extends Component {
 				<h1 class="mt-4">Article</h1>
                 <form>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="headlineInput" placeholder="Headline" />
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="titleInput" placeholder="Title" aria-describedby="titleHelp" />
-                        <small id="titleHelp" class="form-text text-muted">For SEO purposes: goes directly to "title" tag</small>
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control" id="previewInput" rows="6" placeholder="Preview"></textarea>
+                        <input type="text" class="form-control" id="headlineInput" placeholder="Headline" value={this.state.headline} onChange={this.handleHeadlineChange} />
                     </div>
 
                     <div class="form-group">
-                        <textarea class="form-control" id="descriptionInput" rows="3" placeholder="Description" aria-describedby="descriptionHelp"></textarea>
-                        <small id="descriptionHelp" class="form-text text-muted">For SEO purposes: goes directly to "description" tag</small>
+                        <input type="text" class="form-control" id="aliasInput" placeholder="Alias" aria-describedby="aliasHelp" value={this.state.alias} onChange={this.handleAliasChange} />
+                        <small id="aliasHelp" class="form-text text-muted">For SEO purposes: human readable URL</small>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="titleInput" placeholder="Title" aria-describedby="titleHelp" value={this.state.title} onChange={this.handleTitleChange} />
+                        <small id="titleHelp" class="form-text text-muted">For SEO purposes: goes directly to "meta title" tag</small>
+                    </div>
+                    
+                    <div class="form-group">
+                        <textarea class="form-control" id="descriptionInput" rows="3" placeholder="Description" aria-describedby="descriptionHelp" onChange={this.handleDescriptionChange} value={this.state.description}></textarea>
+                        <small id="descriptionHelp" class="form-text text-muted">For SEO purposes: goes directly to "meta description" tag</small>
+                    </div>
+                    
+                    <div class="form-group">
+                        <textarea class="form-control" id="previewInput" rows="6" placeholder="Preview" onChange={this.handlePreviewChange} value={this.state.preview}></textarea>
                     </div>
                     <div class="form-group">
                         <ImageUploader
@@ -68,34 +127,38 @@ class Article extends Component {
                             singleImage = {true}
                             withPreview = {true}
                             withIcon = {true}
-                            buttonText = 'Choose image'
-                            onChange = {this.onDrop}
-                            imgExtension = {['.jpg', '.png', '.gif']}
+                            buttonText = "Choose image"
+                            onChange = {this.handleImageChange}
+                            imgExtension = {[".jpg", ".png", ".gif"]}
                             maxFileSize = {5242880}
                             label = {""}
                         />
                     </div>
                     <div class="form-group">
                         <Editor
-                            initialValue = "<p>Article content</p>"
+                            value = { this.state.content }
                             init = {{
                                 height: 500,
                                 menubar: false,
                                 plugins: [
-                                    'advlist autolink lists link image charmap print preview anchor',
-                                    'searchreplace visualblocks code fullscreen',
-                                    'insertdatetime media table paste code help wordcount'
+                                    "advlist autolink lists link image charmap print preview anchor",
+                                    "searchreplace visualblocks code fullscreen",
+                                    "insertdatetime media table paste code help wordcount"
                                 ],
                                 toolbar:
-                                    'undo redo | formatselect | bold italic backcolor | \
+                                    "undo redo | formatselect | bold italic backcolor | \
                                     alignleft aligncenter alignright alignjustify | \
-                                    bullist numlist outdent indent | removeformat | image | help',
+                                    bullist numlist outdent indent | removeformat | image | help",
                                 
                                 // https://www.tiny.cloud/docs/plugins/image/
-                                images_upload_url: '/api/admin/image/upload'
+                                images_upload_url: "/api/admin/image/upload"
                             }}
                             onChange = { this.handleEditorChange }
                         />
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="activeCheck" onChange={this.handleActiveChange} checked={this.state.active}/>
+                        <label class="form-check-label" for="activeCheck">Active</label>
                     </div>
                     <div class="form-group">
                         <button class="btn btn-lg btn-primary btn-block" onClick={this.handleSave}>Save</button>
