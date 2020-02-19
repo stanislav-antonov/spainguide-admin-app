98683290b392f92f0e6962d14c768dd48661326d
Stanislav Antonov <stanislav.antonov@siemens.com>
2020-02-19 17:08:07 +0100

    Go on

--- src/_services/article.service.js
+++ src/_services/article.service.js
@@ -4,12 +4,16 @@ export const articleService = {
     save(article) {
         const action = article.id ? "update/" + article.id  : "create";
 		const endpointUrl = process.env.REACT_APP_SERVER_URL + "/api/article/" + action;
+        
+        const headers = authHeader();
+        headers["Content-Type"] =  "application/json";
+
         const requestOptions = {
             method: "POST",
-            headers: authHeader(),
+            headers: headers,
             body: JSON.stringify(article),
         };
-    
+        
         return fetch(endpointUrl, requestOptions);
     },
 
